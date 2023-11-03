59cf5ff2cc0c6aa013b15098ec90a122c649c957
PatriciaGonFu <patruskatrus@hotmail.com>
2023-11-03 15:30:37 +0100

    despues de comprobar que funciona

--- movieTest.ts
+++ movieTest.ts
@@ -3,24 +3,25 @@ import { Professional } from "./professional";
 
 const movie1 = new Movie('La princesa prometida',1987,'Estadounidense','Aventuras');
 
-const  actor1 = new Professional('Cary Elwes',61,76,183,false,'Británico',0,'actor');
-const actriz1 = new Professional('Robin Wright',57,63,168,false,'Estadounidense',0,'actriz');
-const actor2 = new Professional('Mandy Patinkin',71,75,182,false,'Estadounidense', 0,'actor');
-const actor3 = new Professional('Chris Sarandon',81,75,183,false,'Estadounidense',0,'actor');
-const actor4 = new Professional('Cristopher Guest',75,75,180,false,'Estadounidense',0,'actor');
-const actor5 = new Professional('Wallace Shawn',80,72,157,false,'Estadounidense',0,'actor');
-const actors = [actor1,actriz1,actor2,actor3,actor4,actor5];
+movie1.actors = [
+new Professional('Cary Elwes',61,76,183,false,'Británico',0,'actor'),
+new Professional('Robin Wright',57,63,168,false,'Estadounidense',0,'actriz'),
+new Professional('Mandy Patinkin',71,75,182,false,'Estadounidense', 0,'actor'),
+new Professional('Chris Sarandon',81,75,183,false,'Estadounidense',0,'actor'),
+new Professional('Cristopher Guest',75,75,180,false,'Estadounidense',0,'actor'),
+new Professional('Wallace Shawn',80,72,157,false,'Estadounidense',0,'actor')
+];
 
-const director = new Professional('Rob Reiner',76,85,178,true,'Estadounidense',0,'director');
+movie1.director = new Professional('Rob Reiner',76,85,178,true,'Estadounidense',0,'director');
 
-const writer = new Professional('William Goldman',108,80,180,true,'Estadounidense',0,'escritor');
-const language = 'Inglés';
-const plataforma = 'Amazon Prime Video';
+movie1.writer = new Professional('William Goldman',108,80,180,true,'Estadounidense',0,'escritor');
+movie1.language = 'Inglés';
+movie1.plataforma = 'Amazon Prime Video';
 
-const isMCU = false;
-const mainCharacterName = 'Princesa Buttercup';
-const producer = 'Rob Reiner';
-const distributor = 'Chantal Lam';
+movie1.isMCU = false;
+movie1.mainCharacterName = 'Princesa Buttercup';
+movie1.producer = 'Rob Reiner';
+movie1.distributor = 'Chantal Lam';
 
 const movieInfo = movie1.showInfoMovie();
 console.log(movieInfo);
